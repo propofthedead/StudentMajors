71272ae826eada80377687bfa89246dd75012506
Foster Stule <camodude5@yahoo.com>
2018-06-19 14:16:30 -0400

    tried to integrate entity framework into the projcet

---
+++ StudentMajors/Migrations/201806191802431_intialize.cs
@@ -0,0 +1,63 @@
+namespace StudentMajors.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class intialize : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Classes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Description = c.String(),
+                        MajorId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Enrolleds",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        StudentId = c.Int(nullable: false),
+                        ClassId = c.Int(nullable: false),
+                        Grade = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Majors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Description = c.String(),
+                        MinSat = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        MajorId = c.Int(nullable: false),
+                        Sat = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+            DropTable("dbo.Majors");
+            DropTable("dbo.Enrolleds");
+            DropTable("dbo.Classes");
+        }
+    }
+}
